5548a0fa3279eb3ddbc3ebff721c34e34b7ba33c
haya <hayasoft@hotmail.com>
2016-08-18 01:33:13 +0900

    v1.0.5 release

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-LD_FLAGS=
+LD_FLAGS="-flto"
+CFLAGS="-Wall -Ofast -march=native -flto"
 
-gcc -Wall -Ofast -march=native -fpic -flto -finput-charset=cp932 -I./ -D IN_SHARED_MODULE -c mod_tcpcast.c
-gcc -Wall -shared -fpic -flto -o mod_tcpcast.so mod_tcpcast.o ${LD_FLAGS}
+gcc ${CFLAGS} -fpic -finput-charset=cp932 -I./ -D IN_SHARED_MODULE -c mod_tcpcast.c
+gcc -Wall -shared -fpic -o mod_tcpcast.so mod_tcpcast.o ${LD_FLAGS}
 rm -f mod_tcpcast.o
